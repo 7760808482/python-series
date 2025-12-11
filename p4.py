aa8dc3d89b3e223cc7e09e32a62b8485344114cb
Darshan M R <143525866+7760808482@users.noreply.github.com>
2025-12-11 22:47:34 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,28 @@
+#python youtube cousre study
+
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)  
+
+
+greeting = "Hello! " * 3
+print(greeting)  
+
+
+
+
+message = "  Hello, World!  "
+print(message.strip()) 
+print(message.upper())  
+print(message.replace("World", "Python"))  
+
+p1=input("Enter the string:")
+print(p1.upper())
+print(p1.lower())
+print(p1.strip())
+
+print("Hello \n world")
+printf("Hello \t world")
+
