38a48c5594199a9506f69a95d8c69e6c67414265
Darshan M R <143525866+7760808482@users.noreply.github.com>
2025-12-14 08:12:05 +0530

    Add files via upload

---
+++ p7.py
@@ -0,0 +1,6 @@
+
+a=25
+b=67
+print(a&b)
+print(a|b)
+print(a^b)
---
+++ p8.py
@@ -0,0 +1,20 @@
+
+
+#Creating a list of 5 items
+l=[6,7,8,9,0]
+#Add a new item to the end of the list and another at the second position.
+l.append(911)
+#Remove the third item from the list.
+l.remove(l[2])
+#Print the list after each operation.
+print(l)
+
+
+#Sort it in descending order.
+number=[65,38,93,86,12,11]
+print(sort(number))
+#Reverse the sorted list and print it.
+number.sort(reverse=True)
+print(number)
+number.reverse()
+print(number)
