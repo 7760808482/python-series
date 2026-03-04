794f7fd6abd19cc542f9d3781f7f3a5bf0eca9e4
Darshan M R <143525866+7760808482@users.noreply.github.com>
2026-03-04 20:47:46 +0530

    Add files via upload

---
+++ p12.py
@@ -0,0 +1,18 @@
+#while loop
+is_failed=True
+i=1
+while is_failed and i<=50:
+    print(f"try again{i}")
+    i=i+1
+
+
+
+is_failed=True
+i=1
+while is_failed and :
+    print(f"try again{i}")
+    i=i+1
+    if i<=100:
+        braek
+
+
