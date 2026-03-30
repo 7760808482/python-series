e781fb6b8f43eda9fc84638157a0e03ecf08b9a7
Darshan M R <143525866+7760808482@users.noreply.github.com>
2026-03-30 12:01:08 +0530

    Add files via upload

--- tuple.py
+++ tuple.py
@@ -1,11 +1,31 @@
-#Create a tuple with 5 elements.
-my_tuple=(1,2,3,4,5)
-#Perform slicing on the tuple to extract the second and third elements.
-print(my_tuple[1:3])
-#Concatenate the tuple with another tuple.
-my_tuple2=(6,7,8,9,10)
-print(my_tuple+my_tuple2)
-#Try to modify one of the elements. What happens?
-my_tuple[0]=1
-print(my_tuple)
-print(type(my_tuple))
+my_list=[5,3,1,2,4]
+my_list.append(6)
+my_list.remove(3)
+my_list.sort()
+my_list.reverse()
+my_list[4]=7
+print(my_list)
+
+list=[2,4,6,8]
+total=0
+for i in list:
+    total+=i         #return the sum of all the numbers in the list
+print(total)
+
+list1=[5,2,9,1,7]
+large=list1[0]       #initialize the variable to the first element of the list 
+for i in list1:
+    if i>large:
+        large=i
+print(large)
+
+
+list2=[1,2,3,4,5,6]
+my_list2=0
+for i in list2:
+    if i%2==0:
+        my_list2+=1       #return the count of even numbers in the list
+print(my_list2)
+
+
+
