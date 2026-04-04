07e1c39d06e5b722511a1a79b73d5f37f10ce580
Darshan M R <143525866+7760808482@users.noreply.github.com>
2026-04-04 22:20:11 +0530

    Add files via upload

---
+++ error.py
@@ -0,0 +1,45 @@
+'''a = 10
+b = "5"
+print(a + b)'''
+a=10
+b=5
+print(a+b)
+
+
+'''list = [1, 2, 3]
+print(list[3])'''
+list=[1,2,3]
+print(list[2])
+
+
+'''for i in range(5)
+    print(i)'''
+for i in range(5):
+    print(i)
+
+
+'''x = int("abc")
+print(x)'''
+x='abc'
+print(x)
+
+
+'''def add(a, b):
+print(a + b)
+
+add(2, 3)'''
+def add(a,b):
+    print(a+b)
+add(2,3)
+
+
+'''import maths
+print(maths.sqrt(16))'''
+import math
+print(math.sqrt(16))
+
+
+'''d = {"a": 1, "b": 2}
+print(d["c"])'''
+d={"a":1,"b":2}
+print(d.get("c",3))
