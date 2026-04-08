d5b0d2acdd95c39ecace1edc09eb80d81004f78d
Darshan M R <143525866+7760808482@users.noreply.github.com>
2026-04-08 21:20:49 +0530

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,12 @@
+import qrcode
+
+print("QR Code Generator")
+print("Created by @chandan")
+
+data = input("Enter input>> ")
+
+qr = qrcode.make(data)
+
+qr.save("C:/Users/chand/Desktop/qrcode.png")
+
+print("QR Code generation successful")
