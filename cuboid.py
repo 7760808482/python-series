407e60b2ab01d0c99d18258070b973e3afa8b380
Darshan M R <143525866+7760808482@users.noreply.github.com>
2026-04-28 21:02:54 +0530

    Add files via upload

---
+++ cuboid.py
@@ -0,0 +1,19 @@
+#cuboid using for loop
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+result=[]
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if i+j+k!=n:
+                 result.append([i,j,k])
+print(result)
+
+
+#cuboid using list comprehension
+print([[i,j,k] for i in range(x+1) for j in range(y+1) for j in range(z+1) if i+j+k!=n])
